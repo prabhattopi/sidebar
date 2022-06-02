61113a9385ebc6b222190a98d9d156dd02b64391
prabhattopi <topi9864@gmail.com>
2022-06-03 02:24:14 +0530

    The updated Sidebar

--- src/comoponets/Sidebar.js
+++ src/comoponets/Sidebar.js
@@ -6,7 +6,7 @@ export const Sidebar = () => {
   return (
     <HStack
       p="10px"
-      width="200px"
+      width="230px"
       height="100vh"
       display="flex"
       align="flex-start"
@@ -17,13 +17,13 @@ export const Sidebar = () => {
         <Text fontSize="18px" fontWeight="600">
           Logo
         </Text>
-        <Flex direction="column" mt="30px" gap="20px">
-          <Flex ml="10px" gap="20px" align="center">
-            <Icon fontSize="20px" as={FaHome}></Icon>
+        <Flex direction="column" mt="30px" gap="20px" width="200px" p="10px" >
+          <Flex  gap="20px" align="center" _hover={{bg:"cyan.500",borderRadius:"5px"}} width="100%" height="60px">
+            <Icon ml="10px" fontSize="20px" as={FaHome}></Icon>
             <Text fontSize="20px">Home</Text>
           </Flex>
-          <Flex ml="10px" gap="20px" align="center">
-            <Icon fontSize="20px" as={FaUpload}></Icon>
+          <Flex  gap="20px" align="center" _hover={{bg:"cyan.500",borderRadius:"5px"}} width="100%" height="60px">
+            <Icon ml="10px" fontSize="20px" as={FaUpload}></Icon>
 
 
           
@@ -32,16 +32,16 @@ export const Sidebar = () => {
 
             <Text fontSize="20px">Trending</Text>
           </Flex>
-          <Flex ml="10px" gap="20px" align="center">
-            <Icon fontSize="20px" as={FaStar}></Icon>
+          <Flex  gap="20px" align="center" _hover={{bg:"cyan.500",borderRadius:"5px"}} width="100%" height="60px">
+            <Icon ml="10px" fontSize="20px" as={FaStar}></Icon>
             <Text fontSize="20px">Favourite</Text>
           </Flex>
-          <Flex ml="10px" gap="20px" align="center">
-            <Icon fontSize="20px"></Icon>
+          <Flex  gap="20px" align="center" _hover={{bg:"cyan.500",borderRadius:"5px"}} width="100%" height="60px">
+            <Icon ml="10px" fontSize="20px"></Icon>
             <Text fontSize="20px">Explore</Text>
           </Flex>
-          <Flex ml="10px" gap="20px" align="center">
-            <Icon fontSize="20px" as={FaArrowRight}></Icon>
+          <Flex  gap="20px" align="center" _hover={{bg:"cyan.500",borderRadius:"5px"}} width="100%" height="60px">
+            <Icon ml="10px" fontSize="20px" as={FaArrowRight}></Icon>
             <Text fontSize="20px">Settings</Text>
           </Flex>
           
